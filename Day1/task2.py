98f68defe54bc3619c3bf84c2c2beca50865eb0e
StuPle <stuart.f.poole@gmail.com>
2019-12-01 21:56:17 +0000

    Vectorised task 2. Calculates masses much faster now.

--- Day1/task2.py
+++ Day1/task2.py
@@ -1,11 +1,9 @@
 import numpy as np
 
 
-# Function to calculate fuel required to move mass according to f = floor(m/3)-2
-def calculate_fuel(m):
+def calculate_fuel_vect(m):
     res = np.floor(m / 3) - 2
-    if res <= 0:
-        res = 0
+    res[res <= 0] = 0
     return res
 
 
@@ -14,12 +12,11 @@ file_token = open("input.txt", "r")
 in_masses = np.loadtxt(file_token)
 file_token.close()
 
-# Loops through each mass calculating each mass required for each module and then the mass required for it's fuel.
+# Vectorised version of calculating masses
 out_mass = 0
-for mass in in_masses:
-    work_mass = mass
-    while work_mass > 0:
-        work_mass = calculate_fuel(work_mass)
-        out_mass += work_mass
+work_mass = in_masses.copy()
+while np.sum(work_mass) > 0:
+    work_mass = calculate_fuel_vect(work_mass)
+    out_mass += np.sum(work_mass)
 
 print(out_mass)
