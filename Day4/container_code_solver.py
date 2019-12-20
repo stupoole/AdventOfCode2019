e00bb40bbc44d868616f253af70fba302447ded2
StuPle <stuart.f.poole@gmail.com>
2019-12-20 15:28:46 +0000

    Commenting day 4

--- Day4/container_code_solver.py
+++ Day4/container_code_solver.py
@@ -1,6 +1,17 @@
-input = range(245182, 790572)
+input_range = range(245182, 790572)
 
 
+#  Returns true if there is atleast a double and the digits are counting up or the same.
+def filter_part1(in_string):
+    a, b, c, d, e, f = in_string
+    a, b, c, d, e, f = int(a), int(b), int(c), int(d), int(e), int(f)
+    ordering = f >= e >= d >= c >= b >= a
+    doubles = a == b or b == c or c == d or d == e or e == f
+
+    return ordering and doubles
+
+
+#  Returns true if there is atleast 1 double and not triple, and counts up.
 def filter_part2(in_string):
     a, b, c, d, e, f = in_string
     a, b, c, d, e, f = int(a), int(b), int(c), int(d), int(e), int(f)
@@ -12,19 +23,10 @@ def filter_part2(in_string):
     return ordering and doubles
 
 
-def filter_part1(in_string):
-    a, b, c, d, e, f = in_string
-    a, b, c, d, e, f = int(a), int(b), int(c), int(d), int(e), int(f)
-    ordering = f >= e >= d >= c >= b >= a
-    doubles = a == b or b == c or c == d or d == e or e == f
-
-    return ordering and doubles
-
-
-input_strings = (str(a) for a in input)
+input_strings = (str(a) for a in input_range)
 filtered_1 = tuple(filter(filter_part1, input_strings))
-input_strings = (str(a) for a in input)
+input_strings = (str(a) for a in input_range)
 filtered_2 = tuple(filter(filter_part2, input_strings))
 
 print("part1: ", len(filtered_1))
-print("part1: ", len(filtered_2))
+print("part2: ", len(filtered_2))
